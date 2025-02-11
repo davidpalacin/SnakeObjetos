04d5df353fc6f3c74f0bca2e5a8aa9646dbbdaff
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2025-02-11 09:32:59 +0100

    Clase Snake completa. Clase Cuadricula completa

--- SnakeObjetos/Partida.cs
+++ SnakeObjetos/Partida.cs
@@ -1,25 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace SnakeObjetos
 {
     internal class Partida
     {
-        // Atributos
-
-        public Partida() { }
-
-        private Snake snake = new Snake();
+        private Snake snake = new Snake(10, 10, 4);
         private Cuadricula cuadricula = new Cuadricula(20, 20);
 
-        // Métodos
         public void IniciarJuego()
         {
-            Console.WriteLine("Bienvenido");
+            Console.Clear();
+            Console.WriteLine("Bienvenido al juego del Snake!");
+            Console.CursorVisible = false;
+
             cuadricula.Dibujar();
+            snake.Dibujar();
+
+            while (true) // Bucle infinito del juego
+            {
+                // Detectar entrada del usuario (sin bloquear)
+                if (Console.KeyAvailable)
+                {
+                    var tecla = Console.ReadKey(true).Key;
+                    switch (tecla)
+                    {
+                        case ConsoleKey.W:
+                        case ConsoleKey.UpArrow:
+                            snake.CambiarDireccion(Direccion.Arriba);
+                            break;
+                        case ConsoleKey.S:
+                        case ConsoleKey.DownArrow:
+                            snake.CambiarDireccion(Direccion.Abajo);
+                            break;
+                        case ConsoleKey.A:
+                        case ConsoleKey.LeftArrow:
+                            snake.CambiarDireccion(Direccion.Izquierda);
+                            break;
+                        case ConsoleKey.D:
+                        case ConsoleKey.RightArrow:
+                            snake.CambiarDireccion(Direccion.Derecha);
+                            break;
+                    }
+                }
+
+                // Mover la serpiente
+                snake.Mover();
+
+                if (snake.HaColisionado(20, 20))  // 20x20 es el tamaño de la cuadrícula
+                {
+                    Console.Clear();
+                    Console.WriteLine("¡Has perdido! Presiona cualquier tecla para salir...");
+                    Console.ReadKey();
+                    break; // Salimos del bucle del juego
+                }
+
+
+                // Redibujar la serpiente
+                Console.Clear();
+                cuadricula.Dibujar();
+                snake.Dibujar();
+
+                // Control de velocidad del juego
+                Thread.Sleep(150);
+            }
         }
     }
 }
--- SnakeObjetos/Snake.cs
+++ SnakeObjetos/Snake.cs
@@ -1,12 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnakeObjetos
 {
     internal class Snake
     {
+        public List<(int x, int y)> Cuerpo { get; private set; }
+        public Direccion DireccionActual { get; private set; }
+        private char Simbolo = 'O';
+
+        public Snake(int xInicial, int yInicial, int longitud)
+        {
+            Cuerpo = new List<(int x, int y)>();
+
+            // Crear la serpiente con la longitud inicial
+            for (int i = 0; i < longitud; i++)
+            {
+                Cuerpo.Add((xInicial - i, yInicial)); // Se genera horizontal hacia la derecha
+            }
+
+            DireccionActual = Direccion.Derecha;
+        }
+
+        public void Mover()
+        {
+            // Obtener la cabeza actual
+            var cabeza = Cuerpo[0];
+            (int x, int y) nuevaCabeza = cabeza;
+
+            // Cambiar la posición de la cabeza según la dirección
+            switch (DireccionActual)
+            {
+                case Direccion.Arriba:
+                    nuevaCabeza.y--;
+                    break;
+                case Direccion.Abajo:
+                    nuevaCabeza.y++;
+                    break;
+                case Direccion.Izquierda:
+                    nuevaCabeza.x -= 2;
+                    break;
+                case Direccion.Derecha:
+                    nuevaCabeza.x += 2;
+                    break;
+            }
+
+            // Insertar la nueva cabeza al inicio de la lista
+            Cuerpo.Insert(0, nuevaCabeza);
+
+            // Eliminar la última posición para simular el movimiento
+            Cuerpo.RemoveAt(Cuerpo.Count - 1);
+        }
+
+        public void Dibujar()
+        {
+            foreach (var parte in Cuerpo)
+            {
+                Console.SetCursorPosition(parte.x, parte.y);
+                Console.Write(Simbolo);
+            }
+        }
+
+        public void CambiarDireccion(Direccion nuevaDireccion)
+        {
+            // Evitar que la serpiente se mueva en dirección opuesta
+            if ((DireccionActual == Direccion.Arriba && nuevaDireccion == Direccion.Abajo) ||
+                (DireccionActual == Direccion.Abajo && nuevaDireccion == Direccion.Arriba) ||
+                (DireccionActual == Direccion.Izquierda && nuevaDireccion == Direccion.Derecha) ||
+                (DireccionActual == Direccion.Derecha && nuevaDireccion == Direccion.Izquierda))
+            {
+                return;
+            }
+
+            DireccionActual = nuevaDireccion;
+        }
+
+        public bool HaColisionado(int ancho, int alto)
+        {
+            var cabeza = Cuerpo[0];
+
+            // Si la cabeza está fuera de los límites, hay colisión
+            if (cabeza.x <= 0 || cabeza.x >= ancho * 2 || cabeza.y <= 0 || cabeza.y >= alto)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public enum Direccion
+    {
+        Arriba,
+        Abajo,
+        Izquierda,
+        Derecha
     }
 }
